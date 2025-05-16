43b6af104e141ddcc5ba6756f07900c3a91186b7
saifakib <mdsaifakib@gmail.com>
2025-05-17 00:06:34 +0600

    feat: swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors()
+  app.use(helmet())
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  
+  const globalPrefix = 'api/v1';
+  app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle("Employee Hierarchy API")
+    .setDescription("API for employee hierarchy information")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build()
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`${globalPrefix}/docs`, app, document);
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
